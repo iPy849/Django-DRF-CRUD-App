6f72d14c0a03b54ecce3c7dd836948913bb58389
alejandroortega <aleortega849@gmail.com>
2022-07-03 19:46:11 -0500

    settings

--- crud_app/settings.py
+++ crud_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=+z!!z#zlg3p@jb934hap38ge^y-#p^g9cfyw7da(6*2)vr_#1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
